110ff6047080bee4951fbec4d92d57273615803e
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-03-30 16:39:38 -0700

    Fixes outline color on hover

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const form = document.getElementById('form');
 
 form.addEventListener('focus', (event) => {
-  event.target.style.outlineColor = "#752A20"
-}, true);
-
-form.addEventListener('blur', (event) => {
-  event.target.style.outlineColor = '';
-}, true);
+    event.target.style.outlineColor = "#752A20"
+    }, true);
+    
+    form.addEventListener('blur', (event) => {
+    event.target.style.outlineColor = '';
+    }, true);
+      
