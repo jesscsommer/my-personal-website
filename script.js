c98d1347c34bec85d81c849f975a6bcc0914ee44
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-04-05 16:47:06 -0700

    Stores form submissions in array

--- script.js
+++ script.js
@@ -18,3 +18,24 @@ button.addEventListener("mouseenter", (event) => {
 button.addEventListener("mouseleave", (event) => {
     event.target.style.backgroundColor = ""
 });
+
+const allFormSubmissions = [];
+
+function storeForm(){
+    const formSubmission = {};
+
+    const nameValue = document.getElementById("name").value;
+    const emailValue = document.getElementById("email").value;
+    const messageValue = document.getElementById("message").value;
+
+    formSubmission.name = nameValue;
+    formSubmission.email = emailValue;
+    formSubmission.message = messageValue;
+    // console.log(formSubmission);
+
+    allFormSubmissions.push(formSubmission);
+
+    for (let i = 0; i < allFormSubmissions.length; i++) {
+        console.log(allFormSubmissions[i]);
+    }
+}
